040937d2e8e71624e344ccc4dce0f64bc76d43df
tmcowley <thomas.cowley19@gmail.com>
2021-02-01 13:53:43 +0000

    new page, who dis?

--- app/src/main/java/app/App.java
+++ app/src/main/java/app/App.java
@@ -17,5 +17,6 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World");
+        get("/yoitworkd", (req, res) -> "hello there");
     }
 }

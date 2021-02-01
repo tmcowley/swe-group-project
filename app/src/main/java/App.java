fac8c7ac2fbeea668ba48969be4e03e52fe1bd7b
tmcowley <thomas.cowley19@gmail.com>
2021-02-01 13:21:12 +0000

    spark quickstart

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -1,13 +1,22 @@
-package .;
+// package .;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+// /**
+//  * Hello world!
+//  *
+//  */
+// public class App 
+// {
+//     public static void main( String[] args )
+//     {
+//         System.out.println( "Hello World!" );
+//     }
+// }
+
+
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
     }
 }

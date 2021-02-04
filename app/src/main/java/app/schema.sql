d0a5f1e6097e5f5cf98f16a69ecdb35bf79ac8eb
herobrinor <34803353+herobrinor@users.noreply.github.com>
2021-02-04 20:06:05 +0800

    Update schema.sql

--- app/src/main/java/app/schema.sql
+++ app/src/main/java/app/schema.sql
@@ -14,7 +14,7 @@ CREATE TYPE eventType AS ENUM ('lecture', 'seminar', 'conference', 'workshop', '
 
 CREATE TABLE participant(
     participantID   SERIAL           NOT NULL,
-    address         INET             NOT NULL,
+    ipaddress       INET             NOT NULL,
     fName           VARCHAR(35)      NOT NULL,
     lName           VARCHAR(35)      NOT NULL,
     sysBan          BOOLEAN          DEFAULT 0,
@@ -23,7 +23,7 @@ CREATE TABLE participant(
 
 CREATE TABLE host(
     hostID          SERIAL           NOT NULL,
-    address         INET             NOT NULL,
+    ipaddress       INET             NOT NULL,
     eAddress        citext UNIQUE    NOT NULL,
     fName           VARCHAR(35)      NOT NULL,
     lName           VARCHAR(35)      NOT NULL,
@@ -37,6 +37,7 @@ CREATE TABLE event(
     title         VARCHAR(32)          NOT NULL,
     description   VARCHAR(128)         NOT NULL,
     type          eventType            NOT NULL,
+    timestamp     TIMESTAMP            NOT NULL,
     eventCode     VARCHAR(4)           CHECK (eventCode ~* '^[A-Z0-9]+$'), -- simulate upper alphanumeric
     FOREIGN KEY (hostID) REFERENCES host(hostID) ON UPDATE CASCADE ON DELETE CASCADE,
     PRIMARY KEY (eventID)
@@ -49,6 +50,8 @@ CREATE TABLE feedback(
     data              VARCHAR(200)   NOT NULL,
     mood              VARCHAR(40)    NOT NULL,
     sentiment         VARCHAR(40)    NOT NULL,
+    anonymous         BOOLEAN        DEFAULT 0,
+    timestamp         TIMESTAMP      NOT NULL,
     FOREIGN KEY (participantID) REFERENCES participant(participantID) ON DELETE CASCADE,
     FOREIGN KEY (eventID) REFERENCES event(eventID) ON DELETE CASCADE,
     PRIMARY KEY (feedbackID)
@@ -56,15 +59,31 @@ CREATE TABLE feedback(
 
 CREATE TABLE template(
     templateID      SERIAL           NOT NULL,
-    eventID         SERIAL           NOT NULL,
     data            VARCHAR(200)     NOT NULL,
     FOREIGN KEY (eventID) REFERENCES event(eventID) ON DELETE CASCADE,
     PRIMARY KEY (templateID)
 );
 
+CREATE TABLE templateInEvent(
+    templateID         SERIAL           NOT NULL,
+    eventID            SERIAL           NOT NULL,
+    FOREIGN KEY (templateID) REFERENCES template(templateID) ON DELETE CASCADE,
+    FOREIGN KEY (eventID) REFERENCES event(eventID) ON DELETE CASCADE,
+    PRIMARY KEY (templateID,eventID)
+);
+
+CREATE TABLE templateInHost(
+    templateID         SERIAL           NOT NULL,
+    hostID             SERIAL           NOT NULL,
+    FOREIGN KEY (templateID) REFERENCES template(templateID) ON DELETE CASCADE,
+    FOREIGN KEY (hostID) REFERENCES host(hostID) ON DELETE CASCADE,
+    PRIMARY KEY (templateID,hostID)
+);
+
 CREATE TABLE participantInEvent(
     participantID      SERIAL           NOT NULL,
     eventID            SERIAL           NOT NULL,
+    muted              BOOLEAN          DEFAULT 0,
     FOREIGN KEY (participantID) REFERENCES participant(participantID) ON DELETE CASCADE,
     FOREIGN KEY (eventID) REFERENCES event(eventID) ON DELETE CASCADE,
     PRIMARY KEY (participantID,eventID)

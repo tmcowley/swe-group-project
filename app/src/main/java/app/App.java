50cc525a0df9af8be19dda150fc1da1f8f046325
tmcowley <thomas.cowley19@gmail.com>
2021-02-11 18:25:53 +0000

    attempted use of velocity

--- app/src/main/java/app/App.java
+++ app/src/main/java/app/App.java
@@ -14,13 +14,57 @@ package app;
 
 import static spark.Spark.*;
 
+import org.apache.velocity.app.*;
+import java.util.*;
+import spark.*;
+import spark.template.velocity.*;
+
+import org.eclipse.jetty.http.*;
+
+import java.net.*;
+import app.*;
+
+import spark.Spark;
+import static spark.Spark.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.*;
+import java.nio.file.Paths;
+
+import org.eclipse.jetty.http.*;
+
 import app.Event;
 
 public class App {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-        get("/yoitworkd", (req, res) -> "hello there");
-        get("/hola", (req, res) -> "holla");
+        get("/", (req, res) -> "Hello World");
+
+        //get("/", servePage);
+    }
+
+    public static Route servePage = (Request request, Response response) -> {
+        Map<String, Object> model = new HashMap<>();
+
+        String[] products = {"beans", "carrots", "pears"};
+        model.put("products", products);
+
+        return render(request, model, "/velocity/index.vm");
+    };
+
+    public static String render(Request request, Map<String, Object> model, String templatePath) {
+        return strictVelocityEngine().render(new ModelAndView(model, templatePath));
+    }
+
+    // TODO: somehow reference location of vm files in /resources/velocity/
+    private static VelocityTemplateEngine strictVelocityEngine() {
+        VelocityEngine configuredEngine = new VelocityEngine();
+        configuredEngine.setProperty("runtime.references.strict", true);
+        configuredEngine.setProperty("resource.loader", "class");
+        configuredEngine.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+        return new VelocityTemplateEngine(configuredEngine);
     }
 
 }

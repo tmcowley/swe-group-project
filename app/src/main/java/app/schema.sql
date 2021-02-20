3c57643bb42b3979bc439706dcb015ef330d980b
tmcowley <thomas.cowley19@gmail.com>
2021-02-20 14:31:33 +0000

    database: adding fields, constraints

--- app/src/main/java/app/schema.sql
+++ app/src/main/java/app/schema.sql
@@ -6,85 +6,112 @@
 
 -- ENUMS, EXTENSIONS
 CREATE EXTENSION citext;
-CREATE TYPE eventType AS ENUM ('lecture', 'seminar', 'conference', 'workshop', 'other');
+CREATE TYPE event_type 
+    AS ENUM 
+    ('lecture', 'seminar', 'conference', 'workshop', 'other');
 
 ------------------
  --   SCHEMA   --
 ------------------
 
 CREATE TABLE participant(
-    participantID   SERIAL           NOT NULL,
-    ipAddress       INET             NOT NULL,
-    fName           VARCHAR(35)      NOT NULL,
-    lName           VARCHAR(35)      NOT NULL,
-    sysBan          BOOLEAN          DEFAULT 0,
-    PRIMARY KEY (participantID)
+    participant_id  SERIAL          NOT NULL,
+    ip_address      INET            NOT NULL,
+    f_name          VARCHAR(35)     NOT NULL,
+    l_name          VARCHAR(35)     NOT NULL,
+    sys_ban         BOOLEAN         DEFAULT 0,
+    PRIMARY KEY (participant_id)
 );
 
 CREATE TABLE host(
-    hostID          SERIAL           NOT NULL,
-    ipAddress       INET             NOT NULL,
-    eAddress        citext UNIQUE    NOT NULL,
-    fName           VARCHAR(35)      NOT NULL,
-    lName           VARCHAR(35)      NOT NULL,
-    sysBan          BOOLEAN          DEFAULT 0,
-    PRIMARY KEY (hostID)
+    host_id         SERIAL          NOT NULL,
+    host_code       VARCHAR(64)
+                    UNIQUE          NOT NULL,
+    ip_address      INET            NOT NULL,
+    e_address       citext UNIQUE   NOT NULL,
+    f_name          VARCHAR(35)     NOT NULL,
+    l_name          VARCHAR(35)     NOT NULL,
+    sys_ban         BOOLEAN         DEFAULT 0,
+    PRIMARY KEY (host_id)
 );
 
 CREATE TABLE event(
-    eventID       SERIAL               NOT NULL,
-    hostID        SERIAL               NOT NULL,
-    templateID    SERIAL               NOT NULL,
-    title         VARCHAR(32)          NOT NULL,
-    description   VARCHAR(128)         NOT NULL,
-    type          eventType            NOT NULL,
-    startTime     TIMESTAMP            NOT NULL,
-    endTime       TIMESTAMP            NOT NULL,
-    eventCode     VARCHAR(4)           CHECK (eventCode ~* '^[A-Z0-9]+$'), -- simulate upper alphanumeric
-    FOREIGN KEY (templateID) REFERENCES template(templateID) ON DELETE CASCADE,
-    FOREIGN KEY (hostID) REFERENCES host(hostID) ON DELETE CASCADE,
-    PRIMARY KEY (eventID)
+    event_id        SERIAL          NOT NULL,
+    host_id         INT             NOT NULL,
+    template_id     INT,
+    title           VARCHAR(32),
+    description     VARCHAR(128),
+    type            event_type,
+    start_time      TIMESTAMP,
+    end_time        TIMESTAMP,
+    event_code      VARCHAR(4) 
+                    UNIQUE          NOT NULL
+                    -- simulate lower-case alphanumeric
+                    CHECK (event_code ~* '^[a-z0-9]+$'), 
+    FOREIGN KEY (template_id) 
+                    REFERENCES template(template_id) 
+                    ON DELETE SET NULL,
+    FOREIGN KEY (host_id) 
+                    REFERENCES host(host_id) 
+                    ON DELETE CASCADE,
+    PRIMARY KEY (event_id)
 );
 
-CREATE TABLE archivedEvent(
-    eventID       SERIAL               NOT NULL,
-    hostID        SERIAL               NOT NULL,
-    totalMood     VARCHAR(40)          NOT NULL,
-    title         VARCHAR(32)          NOT NULL,
-    description   VARCHAR(128)         NOT NULL,
-    type          eventType            NOT NULL,
-    startTime     TIMESTAMP            NOT NULL,
-    endTime       TIMESTAMP            NOT NULL,
-    FOREIGN KEY (hostID) REFERENCES host(hostID) ON DELETE CASCADE,
-    PRIMARY KEY (eventID)
+CREATE TABLE archived_event(
+    event_id        SERIAL          NOT NULL,
+    host_id         INT             NOT NULL,
+    total_mood      VARCHAR(40)     NOT NULL,
+    title           VARCHAR(32)     NOT NULL,
+    description     VARCHAR(128)    NOT NULL,
+    type            eventType       NOT NULL,
+    start_time      TIMESTAMP       NOT NULL,
+    end_time        TIMESTAMP       NOT NULL,
+    FOREIGN KEY (host_id) 
+        REFERENCES host(host_id) 
+        ON DELETE CASCADE,
+    PRIMARY KEY (event_id)
 );
 
 CREATE TABLE feedback(
-    feedbackID        SERIAL         NOT NULL,
-    participantID     SERIAL         NOT NULL,
-    eventID           SERIAL         NOT NULL,
-    data              VARCHAR(200)   NOT NULL,
-    sentiment         VARCHAR(40)    NOT NULL,
-    anonymous         BOOLEAN        DEFAULT 0,
-    timestamp         TIMESTAMP      NOT NULL,
-    FOREIGN KEY (participantID) REFERENCES participant(participantID) ON DELETE CASCADE,
-    FOREIGN KEY (eventID) REFERENCES event(eventID) ON DELETE CASCADE,
-    PRIMARY KEY (feedbackID)
+    feedback_id     SERIAL          NOT NULL,
+    participant_id  INT             NOT NULL,
+    event_id        INT             NOT NULL,
+    data            VARCHAR(200)    NOT NULL,
+    sentiment       VARCHAR(40)     NOT NULL,
+    anonymous       BOOLEAN         DEFAULT 0,
+    time_stamp      TIMESTAMP       NOT NULL,
+    FOREIGN KEY (participant_id) 
+        REFERENCES participant(participant_id) 
+        ON DELETE CASCADE,
+    FOREIGN KEY (event_id) 
+        REFERENCES event(event_id) 
+        ON DELETE CASCADE,
+    PRIMARY KEY (feedback_id)
 );
 
 CREATE TABLE template(
-    templateID      SERIAL           NOT NULL,
-    hostID          SERIAL           NOT NULL,
-    data            VARCHAR(200)     NOT NULL,
-    FOREIGN KEY (hostID) REFERENCES host(hostID) ON DELETE CASCADE,
-    PRIMARY KEY (templateID)
+    template_id     SERIAL          NOT NULL,
+    host_id         INT             NOT NULL,
+    template_code   VARCHAR(6)
+                    UNIQUE          NOT NULL
+                    -- simulate lower-case alphanumeric
+                    CHECK (event_code ~* '^[a-z0-9]+$'),
+    data            VARCHAR(200)    NOT NULL,
+    FOREIGN KEY (host_id) 
+        REFERENCES host(host_id) 
+        ON DELETE CASCADE,
+    PRIMARY KEY (template_id)
 );
 
-CREATE TABLE participantInEvent(
-    participantID      SERIAL           NOT NULL,
-    eventID            SERIAL           NOT NULL,
-    muted              BOOLEAN          DEFAULT 0,
-    FOREIGN KEY (participantID) REFERENCES participant(participantID) ON DELETE CASCADE,
-    FOREIGN KEY (eventID) REFERENCES event(eventID) ON DELETE CASCADE,
-    PRIMARY KEY (participantID,eventID)
+CREATE TABLE participant_in_event(
+    participant_id  INT         NOT NULL,
+    event_id        INT         NOT NULL,
+    muted           BOOLEAN     DEFAULT 0,
+    FOREIGN KEY (participant_id) 
+        REFERENCES participant(participant_id) 
+        ON DELETE CASCADE,
+    FOREIGN KEY (event_id) 
+        REFERENCES event(event_id) 
+        ON DELETE CASCADE,
+    PRIMARY KEY (participant_id,event_id)
 );

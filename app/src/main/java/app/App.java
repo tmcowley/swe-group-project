15c39b025c0c2c8e4e139936f5f0b383c86c4049
tmcowley <thomas.cowley19@gmail.com>
2021-03-01 14:26:40 +0000

    App.java: import cleanup

--- app/src/main/java/app/App.java
+++ app/src/main/java/app/App.java
@@ -1,35 +1,12 @@
 package app;
 
+// Spark-Java packages
 import static spark.Spark.*;
-
-import org.apache.velocity.app.*;
-import org.apache.velocity.runtime.*;
-import org.apache.velocity.app.VelocityEngine;
-import org.apache.velocity.VelocityContext;
-import java.io.StringWriter;
-import java.util.*;
-import spark.*;
-import spark.template.velocity.*;
-
-import org.eclipse.jetty.http.*;
-
-import java.net.*;
-import app.*;
-
 import spark.Spark;
-import static spark.Spark.*;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.*;
-import java.nio.file.Paths;
-
-import org.eclipse.jetty.http.*;
-
-import app.objects.*;
+// Internal packages
 import app.controllers.*;
+import app.objects.*;
 import app.util.*;
 
 public class App {

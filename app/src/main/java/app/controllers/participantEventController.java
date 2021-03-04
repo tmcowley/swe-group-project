a4ef4dbf859631e035edc52f3f1086b53c4280bd
Yang <Yang.Tang.1@live.warwick.ac.uk>
2021-03-04 17:30:16 +0800

    add error handling in interaction

--- app/src/main/java/app/controllers/participantEventController.java
+++ app/src/main/java/app/controllers/participantEventController.java
@@ -13,31 +13,41 @@ import java.sql.SQLException;
 
 
 public class participantEventController {
-    private DbConnection db;
+    private static DbConnection db;
 
-    private Validator validator;
+    private static Validator validator;
 
     /** Serve the participant-event page (GET request) */
     public static Route servePage = (Request request, Response response) -> {
-        
-        String[] name = request.queryParams("participantName").split(" ");
-        
-        try{
-            // instantiate DB connection
-            DbConnection db = new DbConnection();
 
-            Participant participant = db.createParticipant(request.ip(),name[0],name[1]);
-            Event event = db.getEventByCode(request.queryParams("participantCode"));
-            db.addParticipantToEvent(participant.getParticipantID(), event.getEventID());
-        } catch (SQLException e){
-            System.out.println(e.getMessage());
-            //throw e;
+        validator = new Validator();
+        Event event = null;
+
+        if (validator.eventCodeIsValid(request.queryParams("participantCode"))) {
+            String[] name = request.queryParams("participantName").split(" ");
+            
+            try {
+                // instantiate DB connection
+                db = new DbConnection();
+                
+    
+                Participant participant = db.createParticipant(request.ip(),name[0],name[1]);
+                event = db.getEventByCode(request.queryParams("participantCode"));
+                db.addParticipantToEvent(participant.getParticipantID(), event.getEventID());
+            } catch (SQLException e){
+                System.out.println(e.getMessage());
+                //throw e;
+            }
         }
 
-        Map<String, Object> model = new HashMap<>();
-        model.put("eventTitle", event.getTitle());
-        model.put("eventDescription", event.getDescription());
-        return ViewUtil.render(request, model, "/velocity/participant-event.vm");
+        if (validator.isEventValid(event)) {
+            Map<String, Object> model = new HashMap<>();
+            model.put("eventTitle", event.getTitle());
+            model.put("eventDescription", event.getDescription());
+            return ViewUtil.render(request, model, "/velocity/participant-event.vm");
+        }
+        
+        return ViewUtil.notFound;
     };
 
     // public static Route eventEntryPage = (Request request, Response response) -> {

5a194320a4f3a5099fbbac19d5324e78d5d2b121
HazararBeer <alexander.outside@pm.me>
2021-03-06 17:13:07 +0000

    nlp and feedback protyping

--- app/src/main/java/app/FeedbackInstance.java
+++ app/src/main/java/app/FeedbackInstance.java
@@ -1,7 +1,9 @@
-public class Feedback {
+public class FeedbackInstance {
 
     String[] results; //Holds the actual feedback from template queries
     float[] weights; //Holds the weight (for weighted mean) of each result
+    int[] types; //Holds the type of result
+    int[] key; //Holds if a result is a key result
 
 
 }

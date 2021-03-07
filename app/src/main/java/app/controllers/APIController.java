15f4c50facadd5c5c1d4ace6868a148f366bd5e0
tmcowley <thomas.cowley19@gmail.com>
2021-03-07 23:00:50 +0000

    working on API controllers

--- app/src/main/java/app/controllers/APIController.java
+++ app/src/main/java/app/controllers/APIController.java
@@ -21,41 +21,64 @@ public class APIController {
 
     // form sent from front-end to back-end to create host
     public static Route createHost = (Request request, Response response) -> {
-        DbConnection db = new DbConnection();
-        System.out.println("This worked??? \n\n");
-        System.out.println(request.queryParams("hostFName"));
+        System.out.println("createHost API endpoint recognised request \n");
+        Route basePage = HostLoginController.servePage;
+
+        DbConnection db = App.getInstance().getDbConnection();
         String FName = request.queryParams("hostFName");
         String LName = request.queryParams("hostLName");
         String Email = request.queryParams("hostEmail");
         if(v.nameIsValid(FName) && v.nameIsValid(LName) && v.eAddressIsValid(Email)){
-            Host host = App.getInstance().getDbConnection().createHost(FName,LName,"192.168.1.1",Email);
+            Host host = db.createHost(FName,LName,"192.168.1.1",Email);
             String HostCode = host.getHostCode();
         }
         //TODO: get IP
         // 2. create Host, get Host, get Host code
         // 3. send host-code back somehow
-        return null;
+        return basePage;
     };
 
     // form sent by host to create an event
     public static Route createEvent = (Request request, Response response) -> {
-        return null;
+        System.out.println("createEvent API endpoint recognised request \n");
+        Route basePage = null;
+        
+        return basePage;
     };
 
     // form sent by participant to join an event
     public static Route joinEvent = (Request request, Response response) -> {
-        // String FName = request.queryParams("hostFName");
-        // String LName = request.queryParams("hostLName");
-        return null;
+        System.out.println("joinEvent API endpoint recognised request \n");
+        Route basePage = IndexController.servePage;
+
+        // Get form attributes, ensure attributes are valid
+        String FName = request.queryParams("participantFName");
+        String LName = request.queryParams("participantLName");
+        String eventCode = request.queryParams("eventCode");
+        if (!v.nameIsValid(FName)) return basePage;
+        if (!v.nameIsValid(LName)) return basePage;
+        if (!v.eventCodeIsValid(eventCode)) return basePage;
+
+        // create Participant object in DB -> link to event
+
+        // redirect participant to event
+
+        return basePage;
     };
 
     // form sent by user to create participant
     public static Route createParticipant = (Request request, Response response) -> {
-        return null;
+        System.out.println("createParticipant API endpoint recognised request \n");
+        Route basePage = null;
+
+        return basePage;
     };
 
     // form sent by participant (in event) to create an instance of feedback
     public static Route createFeedback = (Request request, Response response) -> {
-        return null;
+        System.out.println("createFeedback API endpoint recognised request \n");
+        Route basePage = null;
+
+        return basePage;
     };
 }

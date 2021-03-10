f2489631398a7fd5fa5445bf154b878532bee612
tmcowley <thomas.cowley19@gmail.com>
2021-03-10 14:07:42 +0000

    fixed bug in Event creation DB method

--- app/src/main/java/app/DbConnection.java
+++ app/src/main/java/app/DbConnection.java
@@ -273,7 +273,7 @@ public class DbConnection{
         try{
             String createEvent = ""
                 + "INSERT INTO event(host_id, title, description, type, start_time, end_time, event_code) "
-                + "VALUES(?, ?, ?, ?::event_type, ?, ?, ?) "
+                + "VALUES(?, ?, ?, ?::type, ?, ?, ?) "
                 + "RETURNING event_id";
             stmt = this.conn.prepareStatement(createEvent);
             stmt.setInt(1, host_id);

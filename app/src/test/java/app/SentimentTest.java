41a51b695e0e3be4994976f3105f0970b3fe39f4
tmcowley <thomas.cowley19@gmail.com>
2021-03-10 15:27:42 +0000

    SentimentTest improvements

--- app/src/test/java/app/SentimentTest.java
+++ app/src/test/java/app/SentimentTest.java
@@ -47,21 +47,19 @@ public class SentimentTest {
         };
         test = new Feedback( 0, 0, results, weights, types, keys, sub_weights, false, timestamp);
         test2 = new Feedback( 0, 0, results, weights, types, keys, sub_weights, false, timestamp);
+
+        try{
+            SentimentAnalyser.main(test);
+            SentimentAnalyser.main(test2);
+        } catch (IOException ioe){
+            assertTrue(false);
+        }
     }
-
-
 
     //TODO - expand feedback object with many more diverse plaintext and non-plaintext
     //TODO - comment properly
     @Test
     public void test_main() throws IOException{
-
-        SentimentAnalyser.main(test);
-        SentimentAnalyser.main(test2);
-        //Perform tests
-        test_main_consistency();
-        test_main_correctness();
-        //est_main_return();
 
         System.out.println(SentimentAnalyser.getCompoundFromText("rooms were dirty, very uncomfortable, and the staff was terrible. They went over and beyond to ignore us. I do not recommend this hotel for anyone visiting downtown. They were extremely unaccommodating and did not allow us to check in early. We got to hotel super early and I didn’t wanna wait. So this was a big minus. The service was very poor as well. Would definitely not send a friend there. The staff at this property are all shit! They all go above and beyond to make your stay uncomfortable. Please give your staff beatings! The worst hotel I’ve ever been privileged enough to stay at. Ugly building, and it only gets more breathtaking when you walk in. Low quality rooms (there was not even a tv by the shower), and low quality service. Also, they are one of few hotels that do not allow people under 21 to book a reservation."));
         System.out.println(SentimentAnalyser.getCompoundFromText("The rooms were clean, very comfortable, and the staff was amazing. They went over and beyond to help make our stay enjoyable. I highly recommend this hotel for anyone visiting downtown. They were extremely accommodating and allowed us to check in early at like 10am. We got to hotel super early and I didn’t wanna wait. So this was a big plus. The service was exceptional as well. Would definitely send a friend there. The staff at this property are all great! They all go above and beyond to make your stay comfortable. Please give your staff awards! The best hotel I’ve ever been privileged enough to stay at. Gorgeous building, and it only gets more breathtaking when you walk in. High quality rooms (there was even a tv by the shower), and high quality service. Also, they are one of few hotels that allow people under 21 to book a reservation."));
@@ -70,13 +68,14 @@ public class SentimentTest {
     }
 
     @Test
-    private void test_main_consistency() throws IOException{
+    public void test_main_consistency() throws IOException{
         assertTrue(test.getWeights().equals(test2.getWeights()));
         assertTrue(test.getCompound().equals(test2.getCompound()));
         assertTrue(test.getKey_Results().equals(test2.getKey_Results()));
     }
 
-    private void test_main_correctness() throws IOException{
+    @Test
+    public void test_main_correctness() throws IOException{
         ArrayList<String> krs = new ArrayList<String>();
         krs.add("Terrible, just terrible.");
         krs.add("3");
@@ -85,7 +84,8 @@ public class SentimentTest {
         System.out.println(test.getCompound());
     }
 
-    private void test_main_return() throws IOException{
+    @Test
+    public void test_main_return() throws IOException{
         //Check feedback has been set correctly
     }
 }

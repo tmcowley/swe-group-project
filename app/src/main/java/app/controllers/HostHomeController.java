77aad6a3dcd6d83c6f33ed780875b5c50c8cec7d
tmcowley <thomas.cowley19@gmail.com>
2021-03-11 14:24:42 +0000

    fixing hostHomeController

--- app/src/main/java/app/controllers/HostHomeController.java
+++ app/src/main/java/app/controllers/HostHomeController.java
@@ -22,26 +22,38 @@ public class HostHomeController {
         DbConnection db = App.getInstance().getDbConnection();
         Validator v = App.getInstance().getValidator();
 
-        // initialise host and collect inputted host-code
-        Host host = null;
-        String hostCode = request.queryParams("hostCode");
-        // validate input before interact with database
-        if (v.hostCodeIsValid(hostCode)) {
-            host = db.getHostByCode(hostCode);
+        request.session(true);
+        if (request.session().isNew()) {
+            return "Error:  HostHomeController:servePage: session does not exist";
         }
-        // return host homepage if host is found
-        if (v.isHostValid(host)) {
-            request.session(true);
-            if (request.session().isNew()) {
-                request.session().attribute("host", host);
-            }
-            Map<String, Object> model = new HashMap<>();
-            model.put("fName", host.getFName());
-            model.put("lName", host.getLName());
-            return ViewUtil.render(request, model, "/velocity/host-home.vm");
+
+        // collect inputted host-code; get host by host-code
+        String hostCode = request.session().attribute("hostCode");
+        Host host = db.getHostByCode(hostCode);
+
+        // validate input before database interaction
+        if (!v.hostCodeIsValid(hostCode)) {
+            return "Error:  HostHomeController:servePage: hostCode invalid";
         }
-        // return notfound if host is not found or hostcode is not valid
-        return "Error: HostHomeController:servePage: host invalid";
+
+        if (!db.hostCodeExists(hostCode)){
+            return "Error:  HostHomeController:servePage: hostCode does not exist";
+        }
+
+        host = db.getHostByCode(hostCode);
+
+        if (!v.isHostValid(host)){
+            return "Error:  HostHomeController:servePage: host invalid";
+        }
+
+        // host valid; store host
+        request.session().attribute("host", host);
+
+        // return host homepage
+        Map<String, Object> model = new HashMap<>();
+        model.put("fName", host.getFName());
+        model.put("lName", host.getLName());
+        return ViewUtil.render(request, model, "/velocity/host-home.vm");
     };
 
 }

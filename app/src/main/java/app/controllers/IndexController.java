31e9d13f9424ee6699d51f421105196626b318c0
tmcowley <thomas.cowley19@gmail.com>
2021-03-11 11:42:03 +0000

    landing page error messages

--- app/src/main/java/app/controllers/IndexController.java
+++ app/src/main/java/app/controllers/IndexController.java
@@ -1,16 +1,27 @@
 package app.controllers;
 
-import java.util.*;
-import java.net.*;
-import java.io.*;
+// for ViewUtil velocity models
+import java.util.HashMap;
+import java.util.Map;
+
 import spark.*;
 import app.util.*;
 
 public class IndexController {
-    /** Serve the index page (GET request) */
+    
+    // Serve the landing (index) page following GET request
     public static Route servePage = (Request request, Response response) -> {
+
+        System.out.println("\nNotice: IndexController:servePage recognized request");
+
+        // start session
+        request.session(true);
+
+        if (request.session().attribute("errorMessageJoinEvent") == null)
+            request.session().attribute("errorMessageJoinEvent", "");
+
         Map<String, Object> model = new HashMap<>();
-        model.put("test1", "this is a test variable!");
+        model.put("errorMessageJoinEvent", request.session().attribute("errorMessageJoinEvent"));
         return ViewUtil.render(request, model, "/velocity/landing.vm");
     };
 

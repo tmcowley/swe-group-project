114b7bf30aae4144b7876622d4dafe68712256c5
tmcowley <thomas.cowley19@gmail.com>
2021-03-13 01:09:48 +0000

    cleanup and test fix

--- app/src/test/java/app/DBConnTest.java
+++ app/src/test/java/app/DBConnTest.java
@@ -19,10 +19,10 @@ public class DBConnTest {
     DbConnection db;
     Validator validator;
 
-    public DBConnTest() throws SQLException{
-        try{
+    public DBConnTest() throws SQLException {
+        try {
             db = new DbConnection();
-        } catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
             throw e;
         }
@@ -30,92 +30,28 @@ public class DBConnTest {
     }
 
     @Test
-    public void testCodeGeneration(){
+    public void testCodeGeneration() {
         // Test ten uniquely generated event codes
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             String event_code = db.generateUniqueEventCode();
             assertTrue(validator.eventCodeIsValid(event_code));
         }
 
         // Test ten unique host codes
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             String host_code = db.generateUniqueHostCode();
-            // java.lang.IllegalArgumentException: bound must be positive
-            // at app.DBConnTest.testCodeGeneration(DBConnTest.java:40)
             assertTrue(validator.hostCodeIsValid(host_code));
         }
 
         // Test ten unique template codes
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             String template_code = db.generateUniqueTemplateCode();
             assertTrue(validator.templateCodeIsValid(template_code));
         }
     }
 
-    //@Test
-    public void testCreationAndDeletion(){
-
-        // create timestamp
-        Timestamp timestamp_now = new Timestamp(System.currentTimeMillis());
-
-        // test host creation
-        Host testHost = db.createHost("testFName", "testLName", "test@test.com");
-        int testHostID = testHost.getHostID();
-
-        // test template creation
-        Template testTemplate = db.createTemplate(testHostID, "template-name", timestamp_now,  null);
-        int testTemplateID = testTemplate.getTemplateID();
-
-        // test participant creation
-        Participant testPart = db.createParticipant("testFName", "testLName");
-        int testPartID = testPart.getParticipantID();
-
-        // test event creation
-        Event testEvent = db.createEvent(testHostID, testTemplateID, "event title", "event desc", "seminar", timestamp_now, timestamp_now);
-        int testEventID = testEvent.getEventID();
-
-        // test archived_event creation
-        ArchivedEvent testArchivedEvent = db.createArchivedEvent(testHostID, "", "event title", "event desc", "seminar", timestamp_now, timestamp_now);
-        int testArchivedEventID = testArchivedEvent.getEventID();
-
-        // test feedback creation
-        Feedback testFeedback = db.createFeedback(testPartID, testEventID, false, new Timestamp(System.currentTimeMillis()), null, null, null, null, null, null, null);
-        int testFeedbackID = testFeedback.getFeedbackID();
-
-        // test participant_in_event creation
-        Boolean partInEvent = db.addParticipantToEvent(testPartID, testEventID);
-
-        assertTrue(testHost.getFName().equals("testFName") && testHost.getLName().equals("testLName") && testHost.getEAddress().equals("test@test.com"));
-        assertTrue(testTemplate.getHostID() == testHostID);
-        assertTrue(testPart.getFName().equals("testFName") && testPart.getLName().equals("testLName") && testPart.getIPAddress().equals("127.0.0.1"));
-        assertTrue(testEvent.getHostID() == testHostID && testEvent.getTemplateID() == testTemplateID && testEvent.getTitle().equals("event title") && testEvent.getDescription().equals("event desc") && testEvent.getType().equals("seminar"));
-        assertTrue(testArchivedEvent.getHostID() == testHostID && testArchivedEvent.getMood().equals("") && testArchivedEvent.getTitle().equals("event title") && testArchivedEvent.getDescription().equals("event desc") && testArchivedEvent.getType().equals("seminar"));
-        assertTrue(testFeedback.getParticipantID() == testPartID && testFeedback.getEventID() == testEventID && testFeedback.getAnonymous() == false);
-
-
-        // clear all testing objects from DB
-        db.deleteHost(testHostID);
-        db.deleteTemplate(testTemplateID);
-        db.deleteParticipant(testPartID);
-        db.deleteEvent(testEventID);
-        db.deleteArchivedEvent(testArchivedEventID);
-        db.deleteFeedback(testFeedbackID);
-        db.removeParticipantFromEvent(testPartID, testEventID);
-
-        // assertions
-        assertFalse(testHost == null);
-        assertFalse(testTemplate == null);
-        assertFalse(testPart == null);
-        assertFalse(testEvent == null);
-        assertFalse(testFeedback == null);
-        assertFalse(partInEvent == null || partInEvent == false);
-
-    }
-
-
-
     @Test
-    public void test_createParticipant(){
+    public void test_createParticipant() {
         // participant dummy data: first and last names
         String f_name = "testFName";
         String l_name = "testLName";
@@ -127,7 +63,7 @@ public class DBConnTest {
         // generate local version using local variables
         int commonPartID = storedParticipant.getParticipantID();
         Participant localParticipant = new Participant(commonPartID, f_name, l_name);
-        
+
         // get stored variant of participant object
         storedParticipant = db.getParticipant(commonPartID);
         assertFalse(storedParticipant == null);
@@ -139,29 +75,8 @@ public class DBConnTest {
         db.deleteParticipant(commonPartID);
     }
 
-    /**
-     * generate a unique host email address
-     * @return unique host email
-     */
-    private String generateUniqueEmail(){
-        String e_address;
-        do {
-            // pick valid email
-            e_address = "test@test.com";
-
-            // append 8-digit length random alphanumeric string to avoid collision
-            e_address = e_address.concat(RandomStringUtils.randomAlphanumeric(8).toLowerCase());
-        } while (db.emailExists(e_address));
-
-        // ensure host email has not collided
-        assertFalse(db.emailExists(e_address));
-
-        // return unique email
-        return e_address;
-    }
-
     @Test
-    public void test_createHost(){
+    public void test_createHost() {
         // host dummy data: first, last names and email
         String f_name = "testFName";
         String l_name = "testLName";
@@ -184,8 +99,8 @@ public class DBConnTest {
         db.deleteHost(commonHostID);
     }
 
-    @Test 
-    public void test_participantInEvent_addParticipantToEvent(){
+    @Test
+    public void test_participantInEvent_addParticipantToEvent() {
         // system object dummy data
         String f_name = "testFName";
         String l_name = "testLName";
@@ -201,12 +116,6 @@ public class DBConnTest {
         Host testHost = db.createHost(f_name, l_name, e_address);
         assertFalse(testHost == null);
         int testHostID = testHost.getHostID();
-
-        // MAKE A TEMPLATE code here
-        // Template testTemplate = db.createTemplate(testHostID, null);
-        // assertFalse(testTemplate == null);
-        // int testTemplateID = testTemplate.getTemplateID();
-        // db.deleteTemplate(testTemplateID);
 
         // generate an event
         Event testEvent = db.createEvent(testHostID, "title", "desc", "lecture", ts, ts);
@@ -233,54 +142,93 @@ public class DBConnTest {
     }
 
     @Test
-    public void test_Bans(){
-        //system object dummy data
+    public void test_Bans() {
+        // system object dummy data
         String f_name = "testFName";
         String l_name = "testLName";
         String e_address = generateUniqueEmail();
         Timestamp ts = new Timestamp(System.currentTimeMillis());
-        //create participant
+
+        // create participant
         Participant testPart = db.createParticipant(f_name, l_name);
         assertFalse(testPart == null);
         int testPartID = testPart.getParticipantID();
-        //create host
+
+        // create host
         Host testHost = db.createHost(f_name, l_name, e_address);
         assertFalse(testHost == null);
         int testHostID = testHost.getHostID();
-        //ban host and participant
+
+        // ban host and participant
         db.banParticipant(testPartID);
         db.banHost(testHostID);
-        //check to see if they were banned
+
+        // check to see if they were banned
         testPart = db.getParticipant(testPartID);
         testHost = db.getHostByCode(testHost.getHostCode());
         assertTrue(testHost.getSysBan());
         assertTrue(testPart.getSysBan());
-        //db cleanup
+
+        // db cleanup
         db.deleteHost(testHostID);
         db.deleteParticipant(testPartID);
     }
 
-    //@Test
-    public void test_templateComponents(){
-        //system object dummy data
+    @Test
+    public void test_templateComponents() {
+        // system object dummy data
         String f_name = "testFName";
         String l_name = "testLName";
         String e_address = generateUniqueEmail();
         Timestamp timestamp_now = new Timestamp(System.currentTimeMillis());
-        //create host
+
+        // create host
         Host testHost = db.createHost(f_name, l_name, e_address);
         assertFalse(testHost == null);
         int testHostID = testHost.getHostID();
-        //create template and template component
-        TemplateComponent tc = db.createTemplateComponent("name", "question", "prompt", null, null, "textResponse");
-        Template testTemplate = db.createTemplate(testHostID, "template-name", timestamp_now,  null);
-        //add component to template and check it has been added
-        db.addComponentToTemplate(tc.getId(), testTemplate.getTemplateID());
-        testTemplate = db.getTemplateByCode(testTemplate.getTemplateCode());
-        assertTrue(db.componentInTemplate(tc.getId(), testTemplate.getTemplateID()));
-        //db cleanup
+
+        // create template and template component
+        TemplateComponent component = db.createTemplateComponent("name", "question", "prompt", null, null,
+                "textResponse");
+        assertFalse(component == null);
+        int component_id = component.getId();
+        Template testTemplate = db.createTemplate(testHostID, "template-name", timestamp_now, null);
+        assertFalse(testTemplate == null);
+        int template_id = testTemplate.getTemplateID();
+        String template_code = testTemplate.getTemplateCode();
+
+        // add component to template and check it has been added
+        Boolean added = db.addComponentToTemplate(component_id, template_id);
+        assertTrue(added == true);
+        testTemplate = db.getTemplateByCode(template_code);
+        assertFalse(testTemplate == null);
+        assertTrue(db.componentInTemplate(component_id, template_id));
+
+        // db cleanup
+        db.deleteTemplateComponent(component_id);
+        db.deleteTemplate(template_id);
         db.deleteHost(testHostID);
-        db.deleteTemplate(testTemplate.getTemplateID());
-        db.deleteTemplateComponent(tc.getId());
+    }
+
+    /**
+     * generate a unique host email address
+     * 
+     * @return unique host email
+     */
+    private String generateUniqueEmail() {
+        String e_address;
+        do {
+            // pick valid email
+            e_address = "test@test.com";
+
+            // append 8-digit length random alphanumeric string to avoid collision
+            e_address = e_address.concat(RandomStringUtils.randomAlphanumeric(8).toLowerCase());
+        } while (db.emailExists(e_address));
+
+        // ensure host email has not collided
+        assertFalse(db.emailExists(e_address));
+
+        // return unique email
+        return e_address;
     }
 }
